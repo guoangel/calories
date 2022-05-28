b9c032a212fe9f5ef4fd671621e227767ee99bd9
angel <fsguoangel@hotmail.com>
2022-05-28 21:33:51 +0800

    Host the App

--- quasar.config.js
+++ quasar.config.js
@@ -134,7 +134,7 @@ module.exports = configure(function (ctx) {
 
     // https://v2.quasar.dev/quasar-cli-webpack/developing-pwa/configuring-pwa
     pwa: {
-      workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
+      workboxPluginMode: 'GenerateSW', // 'InjectManifest' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
 
       // for the custom service worker ONLY (/src-pwa/custom-service-worker.[js|ts])
